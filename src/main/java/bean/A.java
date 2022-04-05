abbaeed878ce76b50202f231d2e92c3ebdbc881c
harshada <harshadasawant22@gmail.com>
2022-04-05 22:58:46 +0530

    Autowire

--- src/main/java/bean/A.java
+++ src/main/java/bean/A.java
@@ -1,4 +1,17 @@
 package bean;
 
 public class A {
+    B b;
+    public A(){System.out.println("a is created");}
+    public B getB() {
+        return b;
+    }
+    public void setB(B b) {
+        this.b = b;
+    }
+    void print(){System.out.println("hello a");}
+    public void display(){
+        print();
+        b.print();
+    }
 }
---
+++ src/main/java/test/AutoWireTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import bean.A;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AutoWireTest {
+    public static void main(String[] args) {
+        ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
+        A a=context.getBean("a",A.class);
+        a.display();
+    }
+}
