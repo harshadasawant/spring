d1d544f5f96449c5a7927d5bab24d82e04ca72a9
harshada <harshadasawant22@gmail.com>
2022-04-07 09:43:06 +0530

    @Required depricated dont use

--- src/main/java/bean/AutoClass.java
+++ src/main/java/bean/AutoClass.java
@@ -5,6 +5,16 @@ import org.springframework.stereotype.Component;
 
 public class AutoClass {
     int id;
+//    String name;
+
+//    @Required --> this is depricated. in this annoatation i must to call set Name value if not set then throw beaninitializationexception
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
 
     public int getId() {
         return id;
